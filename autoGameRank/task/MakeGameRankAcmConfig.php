8a9c69e50ade099fa47cc0e8648319b4ee9926d9
miaochen <miaojingchao@tongzhuogame.com>
2021-03-30 12:53:26 +0800

    添加任务

--- autoGameRank/task/MakeGameRankAcmConfig.php
+++ autoGameRank/task/MakeGameRankAcmConfig.php
@@ -7,41 +7,47 @@ use PhpOffice\PhpSpreadsheet\Reader\Exception;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class MakeGameRankAcmConfig {
-    
-    public static $giftMappingConfig = '../config/lively_market_reward_mapping.json';
-    public static $excelFile = '/Users/zhangyan/Desktop/lively_market_reward_list.xlsx';
-    public static $outputFile = '../acm/lively_market_reward_list.json';
 
-    /**
-     * @return void
-     */
+    public static $excelFile = '/Users/zhangyan/Desktop/game_rank.xlsx';
+    public static $outputFile = '../acm/game_rank.json';
+    public static $gameDouble = '../config/games_double.json';
+    public static $gameSingle = '../config/games_single.json';
+    public $result = [];
+
     public function __construct()
     {
-        $gameRank = [];
-        $gameRankDayGiftConfig = [];
         $inputExcelConfig = self::readExcel();
-        
+
         foreach ($inputExcelConfig as $dayInputConfig) {
             if (empty($dayInputConfig)) {
                 echo '表格存在空行，需检查数据完整性';
                 break;
             }
-            
+
             $excelDateToTimestamp = Date::excelToTimestamp($dayInputConfig[0]);
             $date = date("Y-m-d", $excelDateToTimestamp);
+            $gameType = $dayInputConfig[1] == '对战' ? 'fight' : 'single';
+            $gameId = self::getGameIdByGameName($gameType, $dayInputConfig[2]);
+            $this->formatGameRankConfig($date, $gameId, $gameType);
         }
 
-        self::output($result);
+        self::output($this->result, self::$outputFile);
     }
 
-    /**
-     * 输出
-     */
+    public function formatGameRankConfig($date, $gameId, $gameType)
+    {
+        $this->result[$date] = [
+            "date" => $date,
+            "game_id" => $gameId,
+            "game_type" => $gameType
+        ];
+    }
+
     public static function output(array $output, $file)
     {
-        echo '>> lively_market_reward_list' . PHP_EOL;
+        echo '>> game_rank' . PHP_EOL;
         system("echo '' > $file");
-        file_put_contents($file ,json_encode($gameRank, JSON_PRETTY_PRINT). PHP_EOL, LOCK_EX);
+        file_put_contents($file ,json_encode($output, JSON_PRETTY_PRINT). PHP_EOL, LOCK_EX);
         echo 'OK!' . PHP_EOL;
     }
 
@@ -53,7 +59,7 @@ class MakeGameRankAcmConfig {
     public static function readExcel()
     {
         $excelArray = [];
-    
+
         try {
             $reader = IOFactory::createReaderForFile(self::$excelFile);
             $reader->setReadDataOnly(true);
@@ -61,11 +67,11 @@ class MakeGameRankAcmConfig {
         } catch (Exception $e) {
             die($e->getMessage());
         }
-    
+
         $sheet = $spreadsheet->getActiveSheet();
-    
-        // 从表格第四行开始读取
-        foreach ($sheet->getRowIterator(4) as $row) {
+
+        // 从表格第2行开始读取
+        foreach ($sheet->getRowIterator(2) as $row) {
             $rowTmp = [];
             foreach ($row->getCellIterator() as $cell) {
                 $rowTmp[] = $cell->getFormattedValue();
@@ -74,23 +80,30 @@ class MakeGameRankAcmConfig {
                 $excelArray[$row->getRowIndex()] = $rowTmp;
             }
         }
-    
+
         return $excelArray;
     }
 
-    /**
-     * 获取通用配置文件
-     *
-     * @param [type] $filename
-     * @return array $config
-     */
     public static function getConfigFile($filename)
     {
         if (!is_file($filename)) {
             throw new Exception('File "' . $filename . '" does not exist.');
         }
-        
+
         return json_decode(file_get_contents($filename), true);
+    }
+
+    public static function getGameIdByGameName($gameType, $gameName)
+    {
+        $configFile = $gameType == 'fight' ? self::$gameDouble : self::$gameSingle;
+        $gameConfig = self::getConfigFile($configFile);
+        foreach ($gameConfig as $gameId => $game) {
+            if (trim($game['name']) == $gameName) {
+                return $gameId;
+            }
+        }
+
+        throw new Exception("游戏《". $gameName ."》不存在");
     }
 }
 
